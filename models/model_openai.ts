85545368a953d57cb659603d1f5bae519c869d41
Mario De Luca <bloodcreed21@gmail.com>
2025-02-15 15:38:02 +0100

    fix(openai): updated for reasoning model "o"

--- models/model_openai.ts
+++ models/model_openai.ts
@@ -4,31 +4,36 @@ import { requestUrl } from "obsidian";
 export class OpenAIModel extends ChatInterface {
 
 	async generate(prompt: string): Promise<string> {
+		const requestBody: any = {
+			model: this.modelName,
+			response_format: {
+				type: "text"
+			},
+			messages: [
+				{
+					role: "user",
+					content: [
+						{
+							type: "text",
+							text: prompt
+						}
+					]
+				}
+			]
+		};
+
+		if (this.modelName.includes("gpt")) {
+			requestBody.max_completition_tokens = +this.maxTokens;
+			requestBody.temperature = 0.5
+		}
+
 		const message = await requestUrl({
 			url: "https://api.openai.com/v1/chat/completions",
 			headers: {
 				"Content-Type": "application/json",
 				"Authorization": `Bearer ${this.apiKey}`,
 			},
-			body: JSON.stringify({
-				model: this.modelName,
-				temperature: 0.5,
-				max_completition_tokens: +this.maxTokens,
-				response_format: {
-					type: "text"
-				},
-				messages: [
-					{
-						role: "user",
-						content: [
-							{
-								type: "text",
-								text: prompt
-							}
-						]
-					}
-				]
-			}),
+			body: JSON.stringify(requestBody),
 			method: "POST"
 
 		}).catch((error) => {
